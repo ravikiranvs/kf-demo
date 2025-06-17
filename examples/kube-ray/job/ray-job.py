42bb43fa9fa3d27003ac93a5c478d4d638535e91
ravikiranvs <ravi_kiran_vs@yahoo.co.in>
2025-06-17 09:31:55 +0000

    fix

--- examples/kube-ray/job/ray-job.py
+++ examples/kube-ray/job/ray-job.py
@@ -35,20 +35,25 @@ if __name__ == "__main__":
         # Load base model and merge adapter
         model_name = "Qwen/Qwen2.5-Coder-1.5B"
         base = AutoModelForCausalLM.from_pretrained(model_name)
-        merged = PeftModel.from_pretrained(base, checkpoint_path).merge_and_unload()
+        merged_model = PeftModel.from_pretrained(base, checkpoint_path).merge_and_unload()
         tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-        merged_dir = os.path.join(checkpoint_path, "ft_merged")
-        os.makedirs(merged_dir, exist_ok=True)
-        merged.save_pretrained(merged_dir)
-        tokenizer.save_pretrained(merged_dir)
+        # merged_dir = os.path.join(checkpoint_path, "ft_merged")
+        # os.makedirs(merged_dir, exist_ok=True)
+        # merged_model.save_pretrained(merged_dir)
+        # tokenizer.save_pretrained(merged_dir)
+
+        gen_ai_pipeline = pipeline(
+            "text-generation",
+            model=merged_model,
+            tokenizer=tokenizer,
+            device=0 if torch.cuda.is_available() else -1
+        )
         
         mlflow.set_experiment("finetuned-qwen2.5")
         with mlflow.start_run():
             mlflow.transformers.log_model(
-                transformers_model=merged_dir,
-                artifact_path="transformers_model",
-                task="text-generation",
-                save_pretrained=True
+                transformers_model=gen_ai_pipeline,
+                name="cypher-finetuned-qwen2.5-1b",
             )
             print("Checkpoint saved to MLFlow")
